684c0ec4d100834cd0187efaeae6ecac8da9d2b1
Jovi <imjovi@qq.com>
2025-10-28 23:57:01 +0800

    FillWith增加UPDATE_EVERYTIME枚举

--- flexsql-core/src/main/java/cn/vonce/sql/enumerate/FillWith.java
+++ flexsql-core/src/main/java/cn/vonce/sql/enumerate/FillWith.java
@@ -2,12 +2,28 @@ package cn.vonce.sql.enumerate;
 
 /**
  * 填充类型
+ *
  * @author Jovi
  * @email imjovi@qq.com
  * @date 2022/6/24 21:04
  */
 public enum FillWith {
 
-    INSERT, UPDATE, TOGETHER
+    /**
+     * 新增时如果数据为null则填充默认值
+     */
+    INSERT,
+    /**
+     * 更新时如果数据为nul填则充l默认值
+     */
+    UPDATE,
+    /**
+     * 新增和更新同时，如果数据为nul填则充默认值
+     */
+    TOGETHER,
+    /**
+     * 更新时每次都填充默认值
+     */
+    UPDATE_EVERYTIME
 
 }
